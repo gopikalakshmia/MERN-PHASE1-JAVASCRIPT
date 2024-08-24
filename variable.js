98022f4b14944b6054d0db9460e270d1179e12ef
gopika <gopikalakshmia@gmail.com>
2024-08-24 10:07:30 -0700

    first

---
+++ variable.js
@@ -0,0 +1,21 @@
+let variable="I am variable";
+const constexample="I am const";
+var legacyex="I am a legacy variable";
+
+console.log(variable);
+console.log(constexample);
+console.log(legacyex);
+
+//update
+variable="i have a new value";
+legacyex="i am a new legacy variable";
+///constexample="i am new const";
+
+console.log(variable);
+console.log(constexample);
+console.log(legacyex);
+
+
+//primitive
+let stringEx="Hello Javascript";
+console.log(stringEx);
